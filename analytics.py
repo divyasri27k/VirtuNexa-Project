cb493718c9121d600c7b20c1439a292ecc4e2b88
C.DIVYASRI <167788688+divyasri27k@users.noreply.github.com>
2025-05-30 12:16:42 +0530

    Delete analytics.py

--- analytics.py
+++
@@ -1,9 +0,0 @@
-
-import pandas as pd
-import sqlite3
-
-def generate_report():
-    conn = sqlite3.connect("data/urls.db")
-    df = pd.read_sql_query("SELECT * FROM urls", conn)
-    df.to_csv("logs/url_report.csv", index=False)
-    print("Report saved as 'logs/url_report.csv'")
